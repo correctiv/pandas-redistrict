72cf2a37f7fcfe62a9ac7031c645a572a7d08e92
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2015-08-27 17:32:33 +0200

    Clean up, write some tests

--- redistrict/redistrict.py
+++ redistrict/redistrict.py
@@ -10,6 +10,21 @@ logger = logging.getLogger(__file__)
 
 
 def redistrict(df, kind, start=None, end=None, drop=False, splits=True):
+    """Redistrict dataframe indexed by district
+
+    Args:
+        df (pandas.DataFrame): input dataframe
+        kind (string): identifier of redistrict info (e.g. de/kreise)
+        start (int, datetime or date, optional):
+                Use only redistrict information beginningwith this year.
+        end (int, datetime or date, optional):
+                Use only redistrict information up to and including this year.
+        drop (bool, default: False): Drop rows with old identifiers.
+        splits (bool, default: True): Apply district splitting operations
+
+    Returns:
+        pandas.DataFrame: Redistricted dataframe
+    """
     if isinstance(start, int):
         start = date(start, 1, 1)
     elif isinstance(start, datetime):
@@ -18,19 +33,109 @@ def redistrict(df, kind, start=None, end=None, drop=False, splits=True):
         end = date(end, 12, 31)
     elif isinstance(end, datetime):
         end = end.date()
-
-    path = os.path.join(BASE_DIR, 'data', kind + '.json')
-    data_dict = json.load(open(path))
+    data_dict = get_data_dict(kind)
     return apply_changes(df, data_dict['changes'], start=start, end=end,
                          drop=drop, splits=splits)
 
 
+def redistrict_grouped(df, kind, group_cols, district_col=None,
+                       value_cols=None, **kwargs):
+    """Redistrict dataframe by groups
+
+    Args:
+        df (pandas.DataFrame): input dataframe
+        kind (string): identifier of redistrict info (e.g. de/kreise)
+        group_cols (list): List of column names to group by
+        district_col (string): Name of district column
+        value_cols (list): List of column names with values to operate on
+        **kwargs: see redistrict function
+
+    Returns:
+        pandas.Dataframe: Redistricted dataframe
+    """
+    return pd.concat(redistrict_grouped_dataframe(df, kind, group_cols,
+                     district_col=district_col, value_cols=value_cols,
+                     **kwargs))
+
+
+def redistrict_rows(df, kind, **kwargs):
+    data_dict = get_data_dict(kind)
+    affected_ids = list(get_affected(data_dict['changes'], **kwargs))
+    filtered_df = df[df[kwargs['district_col']].isin(affected_ids)]
+    row_generator = redistrict_rows_generator(filtered_df.iterrows(),
+                                              kind, **kwargs)
+
+    rows = list(row_generator)
+    index, rows = [list(t) for t in zip(rows)]
+    df = pd.DataFrame(rows, index=index)
+    return df
+
+
+def redistrict_rows_generator(row_iterator, kind, **kwargs):
+    for i, row in row_iterator:
+        for new_i, new_row in redistrict_row(i, row, kind, **kwargs).iterrows():
+            yield i, new_row
+
+
+def redistrict_row(index, row, kind, **kwargs):
+    district_col = kwargs.pop('district_col')
+    value_cols = kwargs.pop('value_cols')
+    other_cols = list(set(row.index) - set([district_col] + value_cols))
+    df = row.to_frame().T.set_index(district_col)[value_cols]
+    df[value_cols] = df[value_cols].convert_objects(convert_numeric=True)
+    new_df = redistrict(df, kind, **kwargs)
+    new_df = new_df.dropna()
+    new_df = new_df.reset_index()
+    for o in other_cols:
+        new_df[o] = row[o]
+    return new_df
+
+
+def get_affected(changes, **kwargs):
+    for change in changes:
+        if not is_change_affected(change, kwargs.get('start'), kwargs.get('end')):
+            continue
+        for m in change['mergers']:
+            for o in m['old_ids']:
+                yield o
+        for s in change.get('splits', []):
+            yield s['old_id']
+
+
+def is_change_affected(change, start=None, end=None):
+    change_date = datetime.strptime(change['date'], '%Y-%m-%d').date()
+    if start is not None and start > change_date:
+        return False
+    if end is not None and end < change_date:
+        return False
+    return True
+
+
+def get_data_dict(kind):
+    path = os.path.join(BASE_DIR, 'data', kind + '.json')
+    return json.load(open(path))
+
+
+def redistrict_grouped_dataframe(df, kind, group_cols, district_col=None,
+                                 value_cols=None, **kwargs):
+    grouped = df.groupby(group_cols)
+    for k, gdf in grouped:
+        if not isinstance(k, tuple):
+            k = tuple([k])
+        if callable(kwargs.get('start', None)):
+            kwargs['start'] = kwargs['start'](dict(zip(group_cols, k)))
+        if callable(kwargs.get('end', None)):
+            kwargs['end'] = kwargs['end'](dict(zip(group_cols, k)))
+        gdf_r = redistrict(gdf.set_index(district_col)[value_cols], kind, **kwargs)
+        gdf_r = gdf_r.reset_index()
+        for n, v in zip(group_cols, k):
+            gdf_r[n] = v
+        yield gdf_r
+
+
 def apply_changes(df, changes, start=None, end=None, **kwargs):
     for change in changes:
-        change_date = datetime.strptime(change['date'], '%Y-%m-%d').date()
-        if start is not None and start < change_date:
-            continue
-        if end is not None and end > change_date:
+        if not is_change_affected(change, start, end):
             continue
         df = apply_change(df, change, **kwargs)
     return df
@@ -57,14 +162,14 @@ def merge_series(df, series, index_id):
 def apply_mergers(df, mergers, drop=False):
     for merger in mergers:
         sentinels = merger['old_ids']
-        series = df.ix[sentinels].apply(sum)
-        if len(df.ix[sentinels]):
+        if len(df.ix[sentinels].dropna(how='all')):
+            series = df.ix[sentinels].fillna(0).apply(sum)
             logger.info('Merging %s to %s', sentinels, merger['id'])
             df = merge_series(df, series, merger['id'])
             logger.info('Results: %s', df)
-            if drop:
-                logger.info('Dropping %s', sentinels)
-                df = df.drop(sentinels, errors='ignore')
+        if drop:
+            logger.info('Dropping %s', sentinels)
+            df = df.drop(sentinels, errors='ignore')
     return df
 
 
--- redistrict/test.py
+++ redistrict/test.py
@@ -3,20 +3,59 @@ import os
 
 import pandas as pd
 
-from redistrict import redistrict, BASE_DIR
+from redistrict import redistrict, redistrict_grouped, BASE_DIR
 
 
 class RedistrictTest(unittest.TestCase):
-    def test(self):
+    def test_redistrict(self):
         path = os.path.join(BASE_DIR, 'data', 'de', 'kreise_test.csv')
         df = pd.read_csv(path)
         df.AGS = df.AGS.apply(lambda x: '{:0>5d}'.format(x))
         df = df.set_index('AGS')
         df = df[['value1', 'value2']]
         self.assertEqual(df.ix['15082'].sum(), 27)
-        print(df)
         new_df = redistrict(df[['value1', 'value2']], 'de/kreise',
-                           drop=True,
-                           splits=True)
-        print(new_df)
+                            drop=True, splits=True)
         self.assertEqual(new_df.ix['15082'].sum(), 74.25)
+
+    def test_redistrict_grouped(self):
+        path = os.path.join(BASE_DIR, 'data', 'de', 'kreise_test.csv')
+        df = pd.read_csv(path)
+        df.AGS = df.AGS.apply(lambda x: '{:0>5d}'.format(x))
+        df = df.drop(['name'], axis=1)
+        print df
+        new_df = redistrict_grouped(df, 'de/kreise', ['year'],
+                                    district_col='AGS',
+                                    value_cols=['value1', 'value2'],
+                                    drop=True)
+        print new_df
+        kreis = new_df.AGS == '15082'
+        self.assertEqual(round(new_df[kreis & (new_df.year == 2005)].value1.sum(), 2), 22.44)
+        self.assertEqual(new_df[kreis & (new_df.year == 2005)].value2.sum(), 24.81)
+        self.assertEqual(new_df[kreis & (new_df.year == 2013)].value1.sum(), 13)
+        self.assertEqual(new_df[kreis & (new_df.year == 2013)].value2.sum(), 14)
+
+    def test_redistrict_population(self):
+        path = os.path.join(BASE_DIR, 'data', 'de', '12411-0014.csv')
+        population_orig = pd.read_csv(path, skiprows=5, skipfooter=5,
+                                      delimiter=';', encoding='latin-1',
+                                      engine='python')
+        population_orig.rename(columns={
+            'Unnamed: 0': 'AGS',
+            'Unnamed: 1': 'Kreisname'
+        }, inplace=True)
+        years = range(1996, 2015)
+        year_renames = {'31.12.%d' % (year - 1): year for year in years}
+        years = [2011, 2012, 2013]
+        population_orig.rename(columns=year_renames, inplace=True)
+
+        format_ags = lambda x: '{:0>5d}'.format(x)
+        population_orig['AGS'] = population_orig['AGS'].apply(format_ags)
+        population_orig[years] = population_orig[years].convert_objects(convert_numeric=True)
+        population_orig = population_orig.set_index('AGS')
+        population = redistrict(population_orig[years], 'de/kreise', drop=True)
+        population = pd.merge(population, population_orig[['Kreisname']],
+                              how='left', right_index=True, left_index=True)
+        manual_calc = population_orig.ix[["13001", "13059", "13062"]][2011].sum() + population_orig.ix[['13052']][2011].sum() * 0.18
+        result = population.ix['13075'][2011]
+        self.assertEqual(manual_calc, result)
